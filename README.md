a81c974f9ac9e33c2e8f83bbe57c7b6017060d90
Daniele <daniele.rossi18@studio.unibo.it>
2021-05-10 01:54:31 -0700

    Project init
    Base architecture of 5GS Digital Entity backend service

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # 5GSDigitalEntity
-5GSDigitalEntity
---
+++ backend/controllers/SubscriptionData.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.getSubscriptionInfo = function getSubscriptionInfo (req, res, next, ueID) {
+  res.status(200).json("GET SUBSCRIPTION DATA");
+};
